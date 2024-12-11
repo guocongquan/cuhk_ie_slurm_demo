09eaa380226c872c9589778e5842a68589da6f94
guocongquan <guocong.quan@gmail.com>
2024-12-11 21:30:17 +0800

    Update random num generation job

--- slurm_jobs/random_num_generation_job.sh
+++ slurm_jobs/random_num_generation_job.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=job1                         # Job name
-#SBATCH --partition=GPU-TITANX                  # partition name
-#SBATCH --mail-type=END,FAIL                    # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=USER_NAME@ie.cuhk.edu.hk    # fill in your IE email address for job status notification
-#SBATCH --output=slurm_job_%j.log               # Standard output and error log
-#SBATCH --gres=gpu:1                            # instance of GPU resource required, 1 in most case
+#SBATCH --job-name=job1                     # Job name
+#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=gcquan@ie.cuhk.edu.hk   # fill in your IE email address for job status notification
+#SBATCH --output=slurm_job_%j.log           # Standard output and error log
+#SBATCH --array 1-5
+
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 eval "$(conda shell.bash hook)"
 conda activate demo_env
 
 cd ~/cuhk_ie_slurm_demo/random_num_generation
 python generate_num.py
+sleep 10
