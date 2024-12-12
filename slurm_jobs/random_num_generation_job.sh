60ad0d393f2880618808e85cacd268a346720edd
guocongquan <guocong.quan@gmail.com>
2024-12-12 12:45:51 +0800

    Add files

--- slurm_jobs/random_num_generation_job.sh
+++ slurm_jobs/random_num_generation_job.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=job1                     # Job name
-#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=gcquan@ie.cuhk.edu.hk   # fill in your IE email address for job status notification
-#SBATCH --output=slurm_job_%j.log           # Standard output and error log
+#SBATCH --job-name=job1                        # Job name
+#SBATCH --mail-type=END,FAIL                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=USER_NAME@ie.cuhk.edu.hk   # fill in your IE email address for job status notification
+#SBATCH --output=slurm_job_%A_%a.log           # Standard output and error log
 #SBATCH --array 1-5
-
-echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 eval "$(conda shell.bash hook)"
 conda activate demo_env
 
 cd ~/cuhk_ie_slurm_demo/random_num_generation
-python generate_num.py
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+let max_num=2**$SLURM_ARRAY_TASK_ID
+echo "max num can be generated: " $max_num
+python generate_num_in_range.py $max_num
 sleep 10
