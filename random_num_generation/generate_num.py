3a4df831383e2697b02da1205f82680fc1c5ea93
guocongquan <guocong.quan@gmail.com>
2024-12-12 12:53:00 +0800

    Write a bug

--- random_num_generation/generate_num.py
+++ random_num_generation/generate_num.py
@@ -3,6 +3,6 @@ from numpy import random
 print("start running python code")
 
 a = random.randint(10)
-print("generate a random int: " + str(a))
+print("generate a random int: " + a)
 
 print("finish running python code")
